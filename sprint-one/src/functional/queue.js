b06e8f0a81c500b9aef12bb78fadcafd4373bff9
Calvin See <calvinfrancissee@gmail.com>
2022-02-24 12:59:07 -0800

    Implemented functional style

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,23 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
+  var tail = 0;
+  var head = 0;
 
   someInstance.enqueue = function(value) {
+    storage[head] = value;
+    head++;
   };
 
   someInstance.dequeue = function() {
+    let dequeued = storage[tail];
+    delete storage[tail];
+    tail++;
+    return dequeued;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
