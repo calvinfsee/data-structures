d7eff4ea9c8a260c2e5471732992ddfe98b83b1c
Zynh Ludwig <Zynh0722@gmail.com>
2022-02-24 15:43:00 -0800

    Finish tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,8 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = []; // fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,9 +12,22 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  let newChild = Tree(value);
+  this.children.push(newChild);
 };
 
 treeMethods.contains = function(target) {
+  let contained = false;
+  if (this.value === target) {
+    return true;
+  } else {
+    if (this.children.length > 0) {
+      this.children.forEach((child) => {
+        contained = contained || child.contains(target);
+      });
+    }
+  }
+  return contained;
 };
 
 
