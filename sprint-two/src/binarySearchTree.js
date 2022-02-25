9550d3275d85aa4f655aba77e5ca4add34a55ef6
Calvin See <calvinfrancissee@gmail.com>
2022-02-24 20:26:41 -0800

    Implemented binarySearchTree and hashTable

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,49 @@
 var BinarySearchTree = function(value) {
+  var tree = Object.create(BinarySearchTree.prototype);
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+  return tree;
 };
 
+BinarySearchTree.prototype.insert = function (insertedVal) {
+  if (this.value < insertedVal) {
+    if (this.right === null) {
+      this.right = BinarySearchTree(insertedVal);
+    } else {
+      this.right.insert(insertedVal);
+    }
+  } else {
+    if (this.left === null) {
+      this.left = BinarySearchTree(insertedVal);
+    } else {
+      this.left.insert(insertedVal);
+    }
+  }
+};
+
+BinarySearchTree.prototype.contains = function (target) {
+  let foundMatch = false;
+  if (this.value === target) {
+    return true;
+  } else if (this.value < target && this.right !== null) {
+    foundMatch = this.right.contains(target);
+  } else if (this.value > target && this.left !== null) {
+    foundMatch = this.left.contains(target);
+  }
+  return foundMatch;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function (callback) {
+  callback(this.value);
+  if (this.left !== null) {
+    this.left.depthFirstLog(callback);
+  }
+  if (this.right !== null) {
+    this.right.depthFirstLog(callback);
+  }
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,41 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  if (this._storage.get(index)) {
+    for (let i = 0; i < this._storage.get(index).length; i++) {
+      if (this._storage.get(index)[i][0] === k) {
+        this._storage.get(index)[i][1] = v;
+        return;
+      }
+    }
+    this._storage.get(index).push([k, v]);
+
+  } else {
+    this._storage.set(index, [[k, v]]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index).length > 0) {
+    for (let i = 0; i < this._storage.get(index).length; i++) {
+      if (this._storage.get(index)[i][0] === k) {
+        return this._storage.get(index)[i][1];
+      }
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index)) {
+    for (let i = 0; i < this._storage.get(index); i++) {
+      if (this._storage.get(index)[i][0] === k) {
+        this._storage.get(index).splice(i, 1);
+      }
+    }
+  }
 };
 
 
