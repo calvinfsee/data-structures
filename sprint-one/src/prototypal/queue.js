bb009b5c7997d34ce161841851677dc9fb777c90
Calvin See <calvinfrancissee@gmail.com>
2022-02-24 20:22:26 -0800

    Sprint one complete

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -26,4 +26,25 @@ var queueMethods = {
   }
 };
 
+queueMethods.size = function () {
+  return this.numValues;
+};
 
+queueMethods.enqueue = function (value) {
+  this.storage[this.startIndex + this.size()] = value;
+  this.numValues++;
+};
+
+queueMethods.dequeue = function () {
+  var out;
+
+  if (this.numValues > 0) {
+    out = this.storage[this.startIndex];
+    delete this.storage[this.startIndex];
+    this.numValues--;
+    this.startIndex++;
+  }
+
+
+  return out;
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -20,4 +20,23 @@ var stackMethods = {
   }
 };
 
+stackMethods.size = function() {
+  var total = 0;
 
+  for (let i in this.storage) {
+    total++;
+  }
+
+  return total;
+};
+
+stackMethods.push = function(value) {
+  this.storage[this.size()] = value;
+};
+
+stackMethods.pop = function() {
+  var out = this.storage[this.size() - 1];
+  delete this.storage[this.size() - 1];
+
+  return out;
+};
