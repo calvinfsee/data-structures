86311e611b785e347ceac93ffaf53ff759eeeeca
Calvin See <calvinfrancissee@gmail.com>
2022-02-24 20:31:52 -0800

    Implemented linkedList and Tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,33 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    let newNode = Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+    }
+
+    this.tail = newNode;
   };
 
   list.removeHead = function() {
+    let out = this.head.value;
+    this.head = this.head.next;
+    return out;
   };
 
   list.contains = function(target) {
+    currentNode = this.head;
+
+    while (currentNode !== null) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+
+    return false;
   };
 
   return list;
