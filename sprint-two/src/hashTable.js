a91e70f6894f09e8d706d53fd7b19c1f2c6a167a
Zynh Ludwig <Zynh0722@gmail.com>
2022-02-25 14:14:33 -0800

    Add growth to hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,9 +1,26 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._numValues = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
+  if (this._limit * 0.75 <= this._numValues) {
+    let oldLimit = this._limit;
+    let oldStorage = this._storage;
+    this._limit *= 2;
+    this._storage = LimitedArray(this._limit);
+
+    for (let i = 0; i < oldLimit; i++) {
+      let newThis = this;
+      if (oldStorage.get(i)) {
+        oldStorage.get(i).forEach(function(pair) {
+          newThis.insert(pair[0], pair[1]);
+        });
+      }
+    }
+  }
+
   var index = getIndexBelowMaxForKey(k, this._limit);
   if (this._storage.get(index)) {
     for (let i = 0; i < this._storage.get(index).length; i++) {
@@ -16,6 +33,8 @@ HashTable.prototype.insert = function(k, v) {
   } else {
     this._storage.set(index, [[k, v]]);
   }
+
+  this._numValues++;
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -35,6 +54,7 @@ HashTable.prototype.remove = function(k) {
     for (let i = 0; i < this._storage.get(index).length; i++) {
       if (this._storage.get(index)[i][0] === k) {
         this._storage.get(index).splice(i, 1);
+        this._numValues--;
         break;
       }
     }
